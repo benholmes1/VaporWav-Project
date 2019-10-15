3b275c5e656c136a61ee414c1761a0a80fc666e8
Ben Holmes <ben1.holmes136@gmail.com>
2019-10-15 15:23:39 -0700

    Finished check if object exists

--- VaporWav Files/deployed/s3Checks.php
+++ VaporWav Files/deployed/s3Checks.php
@@ -1,39 +1,15 @@
 <?php
 
     class S3Check {
-        public function checkExists($region, $bucket, $key, $IAM_KEY, $IAM_SECRET) {
-          
-            try {
-                $s3 = new S3Client([
-                    'region'      => $region,
-                    'version'     => '2006-03-01',
-                    'credentials' => [
-                        'key' => $IAM_KEY,
-                        'secret' => $IAM_KEY
-                    ]
-                ]);
-            
-            } catch (Exception $e) {
-                die("Error: " . $e->getMessage());
-            }
-
-            try {
-                $check = $s3->headObject([
-                    'Bucket' => $bucket,
-                    'Key' => $key,
-                ]);
-                if($check) {
-                    $checkKey = 1;
-                }
-                else {
-                    $checkKey = 0;
-                }
-            }
-            catch (S3Exception $e) {
-                echo $e->getMessage();
-                echo "\n";
-            }
-            return $checkKey;
+        public function checkObjectExists($key, $conn) {
+          $checkExistsQuery = "SELECT * FROM images WHERE keyname = '".$key."'";
+          $checkExistsResult = $conn->query($checkExistsQuery);
+          if($checkExistsResult->num_rows == 0) {
+              $check = 0;
+          } else {
+              $check = 1;
+          }
+          return $check;
         }
     }
 ?>
