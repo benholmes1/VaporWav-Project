72dbe5329a5c57f4616622349ed8cd8b5fd19b85
Ben Holmes <ben1.holmes136@gmail.com>
2019-10-17 10:43:26 -0700

    Finished check object exists function

--- VaporWav Files/deployed/s3Checks.php
+++ VaporWav Files/deployed/s3Checks.php
@@ -2,14 +2,21 @@
 
     class S3Check {
         public function checkObjectExists($key, $conn) {
-          $checkExistsQuery = "SELECT * FROM images WHERE keyname = '".$key."'";
-          $checkExistsResult = $conn->query($checkExistsQuery);
-          if($checkExistsResult->num_rows == 0) {
-              $check = 0;
-          } else {
-              $check = 1;
-          }
-          return $check;
+            $driver = new mysqli_driver();
+            $driver->report_mode = MYSQLI_REPORT_ALL;
+
+            try {
+                $checkExistsQuery = "SELECT * FROM images WHERE keyname = '".$key."'";
+                $checkExistsResult = $conn->query($checkExistsQuery);
+                if($checkExistsResult->num_rows == 0) {
+                    $check = 0;
+                } else {
+                    $check = 1;
+                }
+            } catch (mysqli_sql_exception $e) {
+                echo $e->__toString();
+            }
+            return $check;
         }
     }
 ?>
