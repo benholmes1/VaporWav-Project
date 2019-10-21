fbf4bf553cb02785e893832f091efbea13db78bc
Ben Holmes <ben1.holmes136@gmail.com>
2019-10-20 17:04:13 -0700

    Working on canvas upload

--- VaporWav Files/deployed/canvas.php
+++ VaporWav Files/deployed/canvas.php
@@ -16,10 +16,52 @@ require './vendor/autoload.php';
 <main role="main">
     <br>
     <div class="container container-large">
-        <h2>Drawing Canvas</h2>
-        <div style="height:80vh" id="lc"></div>
+      <ul class="nav" style="background-color:mediumpurple">
+        <h2 class="navbar-brand">Drawing Canvas</h2>
+        <li class="nav-item dropdown ml-auto">
+          <a class="nav-link pull-right" data-toggle="dropdown" id="imgDropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i style="color:white;vertical-align:middle" class="fa fa-bars"></i></a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="imgDropdown">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#uploadModal">
+            Upload
+          </button>
+          </div>
+        </li>
+      </ul>
+        <div class="canvasUpload" style="height:80vh" id="lc"></div>
     </div>
 </main>
+
+<div class="modal fade" id="uploadModal" tabindex="-1" role="dialog" aria-labelledby="uploadModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="uploadModalLongTitle">Image Details</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <textarea class="form-control" placeholder="Title . . ." style="width:100%;box-sizing:border-box;resize:none" id="title" name="title" form="canvasSubmit" rows="1" required></textarea>
+          <br>
+          <textarea class="form-control" placeholder="Description . . ." style="width:100%;height:5em;box-sizing:border-box;resize:none" id="desc" name="desc" form="canvasSubmit"></textarea>
+          <br>
+          <textarea class="form-control" placeholder="Add Tags separated by commas. . ." style="width:100%;box-sizing:border-box;resize:none" id="taglist" name="taglist" form="canvasSubmit"></textarea>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <form class="canvasSubmit">
+          <input class="btn" type="submit" data-action="s3Upload" value="Upload">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<form class="canvasSubmit">
+    <input type="submit" data-action="s3Upload" value="Upload">
+</form>
 
 <script src="canvasAssests/react-0.14.3.js"></script>
 <script src="canvasAssests/literallycanvas.js"></script>
@@ -32,5 +74,40 @@ require './vendor/autoload.php';
     strokeWidths: [1, 2, 3, 5, 10, 20, 30]
   });
 </script>
+
+<script>
+  $(document).ready(function(){
+    //var lc = LC.init(document.getElementsByClassName('canvasUpload')[0]);
+    $('[data-action=s3Upload]').click(function(e) {
+      e.preventDefault();
+
+      var title = document.getElementById("title").value;
+      var desc = document.getElementById("desc").value;
+      var taglist = document.getElementById("taglist").value;
+
+      alert(title);
+
+      $('.canvasSubmit').html('Uploading...')
+
+      $.ajax({
+        url: 'canvasClass.php',
+        type: 'POST',
+        data: {
+          // convert the image data to base64
+          image:  lc.canvasForExport().toDataURL().split(',')[1],
+          type: 'base64',
+          title: title,
+          desc: desc,
+          taglist: taglist
+        },
+        success: function(result) {
+          alert(result);
+          $('.canvasSubmit').html('Uploaded')
+        },
+      });
+    });
+  });
+</script>
+
 </body>
 </html>
---
+++ VaporWav Files/deployed/canvasClass.php
@@ -0,0 +1,62 @@
+<?php
+
+//This is the upload action that uploads an image to S3 and updates database
+include 'dbconn.php';
+include 'queries.php';
+//Check if user is logged in
+if(!($_SESSION['login'])){
+  header('Location: index.php');
+  exit();
+}
+//This is needed to use AWS SDK for PHP
+require './vendor/autoload.php';
+
+//S3Client for use in upload
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+// AWS Info
+$bucketName = BUCKET_NAME;
+$IAM_KEY = ACCESS_KEY;
+$IAM_SECRET = SECRET_KEY;
+
+// Connect to AWS
+try {
+    $s3 = S3Client::factory(
+        array(
+            'credentials' => array(
+                'key' => $IAM_KEY,	
+                'secret' => $IAM_SECRET
+            ),
+            'version' => 'latest',
+            'region'  => 'us-west-1'
+        )
+    );
+} catch (Exception $e) {
+    die("Error: " . $e->getMessage());
+}
+
+if(isset($_POST['image'])) {
+    $image = $_POST['image'];
+    $imageToUpload = base64_decode($image);
+}
+
+try {
+    // Uploaded:
+    $result = $s3->putObject(
+      array(
+          'Bucket'=>$bucketName,
+          'Key' =>  "thisisatest.png",
+          'Body' => $imageToUpload,
+      )
+    );
+
+    $eTag = $result['ETag'];
+      $vID = $result['VersionId'];
+  } catch (S3Exception $e) {
+    die('Error:' . $e->getMessage());
+  } catch (Exception $e) {
+    die('Error:' . $e->getMessage());
+  }
+
+echo $_POST['title'] . ", " . $_POST['desc'] . ", " . $_POST['taglist'];
